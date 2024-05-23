231095faecbe8e855608cc1ac32fec8bc479b8ca
Diki Akbar <57055952+dikigambol@users.noreply.github.com>
2024-05-23 19:06:58 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Yanuar');
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
