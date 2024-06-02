1f11dd9d08f8374110e981f59232b117e8a417a8
Diki Akbar <57055952+dikigambol@users.noreply.github.com>
2024-06-02 18:18:37 +0700

    updates db

--- hrd_jwt_rsa.sql
+++ hrd_jwt_rsa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Bulan Mei 2024 pada 12.48
+-- Waktu pembuatan: 02 Jun 2024 pada 13.18
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 7.4.29
 
@@ -52,7 +52,10 @@ CREATE TABLE `detail_user` (
 --
 
 INSERT INTO `detail_user` (`tgl_masuk`, `gender`, `no_ktp`, `no_hp`, `email`, `alamat_domisili`, `alamat_ktp`, `tempat_lahir`, `tgl_lahir`, `status_nikah`, `tgl_keluar`, `pendidikan`, `posisi`, `divisi`, `jabatan`, `no_pegawai`, `id_detail`) VALUES
-('2024-01-01', 'Laki-Laki', '1234567890123456', 2147483647, 'yanuar@gmail.com', 'Jalan Merdeka No. 123', 'Jalan Jendral Sudirman No. 456', 'Jakarta', '1990-05-15', 'Single', NULL, 'S1', 'Karyawan', 'UPT', 'Senior Engineer', 1234, 1);
+('2024-01-01', 'Laki-Laki', '1234567890123456', 2147483647, 'yanuar@gmail.com', 'Jalan Merdeka No. 123', 'Jalan Jendral Sudirman No. 456', 'Jakarta', '1990-05-15', 'Single', NULL, 'S2', 'Karyawan', '3', 'Senior Engineer', 1234, 1),
+('2024-06-02', 'Laki-Laki', '12341312', 12313123, 'diki@asia.ac.id', 'Jalan Merdeka No. 12', 'Jalan Jendral Sudirman No. 45', 'Malang', '2024-06-02', 'Menikah', NULL, 'SMA/SMK', 'Karyawan', '4', 'Tidur Saja', 4567, 2),
+('2024-06-02', 'Laki-Laki', '123456', 123456, 'westlakewetland@gmail.com', 'Jalan Merdeka No. 12', 'Jalan Jendral Sudirman No. 456', 'Pasuruan', '2024-06-12', 'Single', NULL, 'S3', 'Dosen', '4', 'OB', 1324, 3),
+('2024-06-28', 'Perempuan', '4564573452', 1212313434, 'upt_si@asia.ac.id', 'Jalan Merdeka No. 12321', 'Jalan Jendral Sudirman No. 4567', 'Jakarta', '2024-06-29', 'Menikah', NULL, 'S2', 'Dosen', '3', 'Senior Engineer Tapi Boong', 9876, 5);
 
 -- --------------------------------------------------------
 
@@ -64,6 +67,14 @@ CREATE TABLE `divisi` (
   `id_divisi` int(10) NOT NULL,
   `ket_divisi` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `divisi`
+--
+
+INSERT INTO `divisi` (`id_divisi`, `ket_divisi`) VALUES
+(3, 'HRD'),
+(4, 'UPT-SI');
 
 -- --------------------------------------------------------
 
@@ -91,6 +102,13 @@ CREATE TABLE `struktural` (
   `atasan` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `struktural`
+--
+
+INSERT INTO `struktural` (`id_struktural`, `id_divisi`, `atasan`) VALUES
+(3, 4, 1324);
+
 -- --------------------------------------------------------
 
 --
@@ -111,7 +129,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `no_pegawai`, `nama`, `password`, `default_password`, `level_user`) VALUES
-(5, 1234, 'yanuar', '$2a$10$DHqBs3DHXG3Ty6HWExbrieOXI3ikeRVlU080jiwr4Y9rIJXaDOq7W', '3e17tl', 'superadmin');
+(5, 1234, 'Yanuar', '$2a$10$DHqBs3DHXG3Ty6HWExbrieOXI3ikeRVlU080jiwr4Y9rIJXaDOq7W', '3e17tl', 'superadmin'),
+(9, 4567, 'Nur Akbar', '$2a$10$pgMR/ynfpnn88m3siOUByuZUv6wjzqg.fEKpzX70GmiPn55GQE1wC', 'r0ydvy', 'user'),
+(13, 1324, 'Akbar Telolet', '$2a$10$QdZr.ctJQ9Z3LWGne/ZIouSDIs6fNVHlU.oAf4s0Jaytp1m2qXff6', 'bpnzr9', 'user'),
+(14, 9876, 'Kasman Gemink', '$2a$10$eLyNd//ClrjfpE44ecNnKeMcgAY03Y4NPzpYB1msW.RDQz9yXWMye', 'fbyly0', 'superadmin');
 
 --
 -- Indexes for dumped tables
@@ -142,7 +163,7 @@ ALTER TABLE `izin_pegawai`
 --
 ALTER TABLE `struktural`
   ADD PRIMARY KEY (`id_struktural`),
-  ADD KEY `relation_1` (`id_divisi`),
+  ADD UNIQUE KEY `id_divisi` (`id_divisi`),
   ADD KEY `relation_2` (`atasan`);
 
 --
@@ -160,13 +181,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `detail_user`
 --
 ALTER TABLE `detail_user`
-  MODIFY `id_detail` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_detail` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `divisi`
 --
 ALTER TABLE `divisi`
-  MODIFY `id_divisi` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_divisi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `izin_pegawai`
@@ -178,13 +199,13 @@ ALTER TABLE `izin_pegawai`
 -- AUTO_INCREMENT untuk tabel `struktural`
 --
 ALTER TABLE `struktural`
-  MODIFY `id_struktural` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_struktural` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
